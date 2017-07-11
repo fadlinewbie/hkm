fec742a253092dfae8367877b54cc2d4b7197f44
fadlinewbie <fadlinewbie@users.noreply.github.com>
2017-07-11 15:19:55 +0700

    hkm
    index.php

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# hkm
+# Sistem Informasi Prakerin (Praktek Kerja Industri) SMK Berbasis Web
+
+## Requirements :
+1. PHP >= 5.4
+2. Database Mysql
+
+## Components :
+1. SpryAssets
+2. CKEditor
+3. Nivo Slider
+4. jQuery Datepicker
+
+## Plugins :
+1. PHPExcel
+2. FPDF17
+
+&copy; 2014 Muhammad Khoirul Umam (umamscarlet)
---
+++ nivo_slider.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="slideshow/styles/default/default.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="slideshow/styles/nivo-slider.css" type="text/css" media="screen" />
+<script type="text/javascript" src="slideshow/jscript/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="slideshow/jscript/jquery.nivo.slider.pack.js"></script>
+<script type="text/javascript"> $(window).load(function() { $('#slider').nivoSlider(); }); </script>
+
+	<div class="slider-wrapper theme-default">
+    	<div class="ribbon"></div>
+        <div id="slider" class="nivoSlider">
+			<img src="images/slideshow/1.jpg" alt="1.jpg" title="Gedung UNSERA Terpadu" />
+			<img src="images/slideshow/2.jpg" alt="2.jpg" title="Bakti sosial HMSI" />
+			<img src="images/slideshow/3.jpg" alt="3.jpg" title="Himpunan Mahasiswa Sistem Komputer" />
+        	<img src="images/slideshow/4.jpg" alt="4.jpg" title="English Day Club Unsera" />
+			<img src="images/slideshow/5.jpg" alt="5.jpg" title="Kore IT Volunteer" />		
+        </div>
+    </div>
+
